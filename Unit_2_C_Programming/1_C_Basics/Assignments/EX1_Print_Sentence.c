126b89e1f9dbd3ff87732153ed9441986a511f05
AshrafWael <ashrafwael368@gmail.com>
2024-01-13 23:03:17 +0200

    add EX1

---
+++ Unit_2_C_Programming/1_C_Basics/Assignments/EX1_Print_Sentence.c
@@ -0,0 +1,15 @@
+/*
+ * print.c
+ *
+ *  Created on: Jan 12, 2024
+ *      Author: Ashraf
+ Description : Unit 2 C-Progtamming >> Assignment 1 C-Basic >> EX1 C Program To Print C Programin using Ansi-c (c98)
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("C Programming");
+	return 0;
+}
