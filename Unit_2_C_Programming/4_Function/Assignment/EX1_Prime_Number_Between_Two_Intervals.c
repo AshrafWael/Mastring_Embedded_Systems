a1373c3cc6d871241e039f37e528a450dcf6f6e7
AshrafWael <ashrafwael368@gmail.com>
2024-01-31 04:01:22 +0200

    add empty file

---
+++ Unit_2_C_Programming/4_Function/Assignment/EX1_Prime_Number_Between_Two_Intervals.c
@@ -0,0 +1,18 @@
+/*
+ * EX1_Prime_Number_Between_Two_Intervals.c
+ *
+ *  Created on: Jan 31, 2024
+ *  Author: Ashraf Wael
+ *  Description : Unit 2 C-Progtamming >> Assignment C-Function >>Prime_Number_Between_Two_Intervals
+ 	 	 	   using Ansi-style (c99)
+
+ */
+#include "stdio.h"
+
+int main(void)
+{
+	
+	
+	
+	return 0;
+}
